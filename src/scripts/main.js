58dd58a076dda2f103876a765dcf42bbae1448b9
raphael <raphaelcardoso@email.com>
2021-09-11 12:12:15 -0300

    :art: Melhora a estrutura do código

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,57 @@ const btn = document.querySelector("#btn")
 
 btn.onclick = function() {
     let username = document.querySelector("#get-username").value
-    let url = "https://api.github.com/users/"+username
+    let url = `https://api.github.com/users/${username}`
 
     fetch(url).then(res=>res.json()).then(data=> {
         if(data.message) {
             alert(`Não foi encontrado o perfil ${username} no github`)
         } else {
             document.querySelector(".user-box").classList.add("fade-in")
-            document.querySelector(".icon-area").innerHTML = `<img src="${data.avatar_url}" alt="${data.name}" class="user-icon">`
-            document.querySelector(".user-name").innerHTML =` ${data.name}`
-            document.querySelector(".joined").innerHTML = `Entrou no GitHub em  ${data.created_at}`
-            document.querySelector(".user-username").innerHTML = `<a href="https://github.com/${data.login}" target="_blank" title="${data.login}"> @${data.login} </a>`
-            document.querySelector(".user-bio").innerHTML = `${data.bio}`
-            document.querySelector(".github-repos").innerHTML = `${data.public_repos}`
-            document.querySelector(".github-seguidores").innerHTML = `${data.followers}`
-            document.querySelector(".github-seguindo").innerHTML = `${data.following}`
+            document.querySelector(".user-box").innerHTML = /*html*/`
+                <div class="icon-area">
+                    <img src="${data.avatar_url}" alt="${data.name}" class="user-icon">
+                </div>
+
+                <div class="user-infos">
+                    <div class="user-infos-header">
+
+                        <span class="user-name">
+                            ${data.name}
+                        </span>
+                        
+                        <span class="joined">
+                            Entrou no GitHub em  ${data.created_at}
+                        </span>
+
+                    </div>
+                    
+                    <p class="user-username">
+                        <a href="https://github.com/${data.login}" target="_blank" title="${data.login}"> @${data.login} </a>
+                    </p>
+
+                    <p class="user-bio">
+                        ${data.bio}
+                    </p>
+
+                    <div class="github-infos">
+                        <div class="box">
+                            <span class="box-title">Repositórios</span>
+                            <span class="github-repos">${data.public_repos}</span>
+                        </div>
+
+                        <div class="box">
+                            <span class="box-title">Seguidores</span>
+                            <span class="github-seguidores">${data.followers}</span>
+                        </div>
+
+                        <div class="box">
+                            <span class="box-title">Seguindo</span>
+                            <span class="github-seguindo">${data.following}</span>
+                        </div>
+                    </div>
+                </div>
+            `
         }
     })
 
